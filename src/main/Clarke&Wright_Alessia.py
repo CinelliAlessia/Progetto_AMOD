2b8febc36f0d5947d7d2ed041772a37159200d21
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-04 10:31:39 +0200

    CW funzionante.

--- src/main/Clarke&Wright_Alessia.py
+++ src/main/Clarke&Wright_Alessia.py
@@ -1,17 +1,17 @@
 from src.main.ParseInstances import work_on_instance
 
 
-nameInstance = "resources/vrplib/Instances/A-n32-k5.vrp"
+nameInstance = "resources/vrplib/Instances/P-n16-k8.vrp"
 #A-n32-k5 P-n16-k8
 nodes, trunk = work_on_instance(nameInstance)
 roots = []  # Strada percorsa da ciascun veicolo matrice N*N con N numero di clienti [[],[],[]]
 
 
+#
 def check_merge(r_i, r_j, capacity):
     # Controllo se la capacità del veicolo è rispettata
-    if sum([nodes[i].get_demand() for i in r_i[0:-2]]) + sum([nodes[i].get_demand() for i in r_j[1:-1]]) > capacity:
+    if sum([nodes[i].get_demand() for i in r_i[1:-1]]) + sum([nodes[i].get_demand() for i in r_j[1:-1]]) > capacity:
         return False
-
     return True
 
 
@@ -32,7 +32,6 @@ def start():
         for v in nodes:
             if u.get_id() != v.get_id():
                 costo[u.get_id()][v.get_id()] = u.get_distance()[v.get_id()]
-                print(f"Costo tra {u.get_id()} e {v.get_id()}: {u.get_distance()[v.get_id()]}")
 
     # Creo le root iniziali
     for c in clients:
@@ -49,7 +48,7 @@ def start():
                 # Se ho unito due roots, ricomincio il ciclo while
                 break
         else:
-            # Se sono qui non ci sono più root da mergiare
+            # Se sono qui non ci sono più root da unire
             go = False
     return roots
 
@@ -64,17 +63,18 @@ def calculate_saving(costo):
                 v = r_j[1]
                 saving.append([u, v, costo[0][u] + costo[v][0] - costo[u][v]])
     saving = sorted(saving, key=lambda x: x[2], reverse=True)
+    print(saving)
     return saving
 
 
-def mergedRoots(saving, trunk):
+def mergedRoots(saving, truck):
     for r_i in roots:
         if r_i[1] == saving[0]:
             for r_j in roots:
                 if r_j[-2] == saving[1]:
                     # Ho trovato le due root che sarebbero utili unire
                     # Controllo se posso unirle
-                    if check_merge(r_i, r_j, trunk.capacity):
+                    if check_merge(r_i, r_j, truck.get_capacity()):
                         print(f"Merge tra {r_i} e {r_j}")
                         roots.remove(r_i)
                         roots.remove(r_j)
@@ -84,5 +84,20 @@ def mergedRoots(saving, trunk):
 
 
 roots_final = start()
+demands_roots = []
+total_cost = []
+
 for r in roots_final:
-    print(r)
+    demand = 0
+    for i in r:
+        demand = demand + nodes[i].get_demand()
+    demands_roots.append(demand)
+    print(f"Route: {r} - Demand: {demand}")
+
+for r in roots_final:
+    cost = 0
+    for i in range(len(r)-1):
+        cost = cost + nodes[i].get_distance()[i+1]
+    demands_roots.append(cost)
+
+print(f"Total demand: {sum(demands_roots)}")
