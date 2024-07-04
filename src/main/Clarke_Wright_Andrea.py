334be30f06b7d4f23b8594bfc8b2bc80a6953ca1
andreatozzi99 <andreatozzi199@gmail.com>
2024-07-04 14:53:02 +0200

    CW apparentemente corretto

--- src/main/Clarke_Wright_Andrea.py
+++ src/main/Clarke_Wright_Andrea.py
@@ -14,41 +14,52 @@ Uno che inizia con ${(0,j)}$ Uno che termina con ${(i,0)}$ Combina questi due pe
 e introducendo ${(i, j)}$.
 """
 import os
-
-import vrplib
-import ParseInstances as parser
+import ParseInstances as Parser
 
 
 # Utilizzando i metodi definiti in ParseInstances.py,
 # calcola i risparmi per ogni coppia di nodi e li ordina in modo decrescente
 def calculate_saves_and_sort_descent(instance):
-    dist = parser.get_edge_weight(instance)
-    depots = parser.get_depots_index(instance)
-    depotIndex = depots[0]  # !!!!! Assunto inizialmente un solo deposito, per semplicità di ragionamento
-    n = parser.get_nodes_dimension(instance)
+    dist = Parser.get_edge_weight(instance)
+    depots = Parser.get_depots_index(instance)
+    depot_index = depots[0]  # todo !!!!! Assunto inizialmente un solo deposito, per semplicità di ragionamento
+    n = Parser.get_nodes_dimension(instance)
     saves = []
-    for i in range(1, n):
+    for i in range(0, n):
         for j in range(i + 1, n):
-            # formato save: (nodo, nodo, valore)
-            saves.append((i, j, dist[depotIndex][i] + dist[j][depotIndex] - dist[i][j]))
+            # formato di un save: (nodo, nodo, valore)
+            # deve essere calcolato solo se i e j non sono depositi
+            if i not in depots or j not in depots:
+                saves.append((i, j, dist[depot_index][i] + dist[j][depot_index] - dist[i][j]))
     saves.sort(key=lambda x: x[2], reverse=True)
     return saves
 
 
-def merge_routes_if_possible(routes, i, j, demands, truck_capacity, edges):
+def merge_routes_if_possible(routes, i, j, demands, truck_capacity):
     route_i = None
     route_j = None
+    # Trova i due percorsi che hanno i come ultimo e j come primo, o viceversa
     for route in routes:
-        if route[-2] == i:  # Il penultimo nodo, perché l'ultimo è il deposito
+        if route[-2] == i or route[1] == i:  # Il penultimo nodo, perché l'ultimo è il deposito
             route_i = route
-        elif route[1] == j:  # Il secondo nodo, perché il primo è il deposito
+        elif route[-2] == j or route[1] == j:  # Il penultimo nodo, perché l'ultimo è il deposito
             route_j = route
+
     if route_i and route_j and route_i != route_j:
         # Calcola la domanda totale per il percorso unito
+        new_route = None
         total_demand = sum(demands[node] for node in route_i[1:-1] + route_j[1:-1])
         if total_demand <= truck_capacity:
-            # Unisci i percorsi
-            new_route = route_i[:-1] + route_j[1:]
+            # Unisci i percorsi, Devo differenziare i due casi:
+            # 1. Se i è l'ultimo nodo di una route e j è il primo nodo di una route
+            if route_i[-2] == i and route_j[1] == j:
+                # prima route_i(tranne dep finale) e poi route_j(tranne dep iniziale)
+                new_route = route_i[:-1] + route_j[1:]
+            # 2. Se j è l'ultimo nodo di una route e i è il primo nodo di una route
+            else:
+                # prima route_j(tranne dep finale) e poi route_i(tranne dep iniziale)
+                new_route = route_j[:-1] + route_i[1:]
+            # Aggiorno la lista delle route
             routes.remove(route_i)
             routes.remove(route_j)
             routes.append(new_route)
@@ -82,48 +93,53 @@ def save_results_to_file(routes, cw_cost, path):
 # Implementazione Euristica di Clarke e Wright
 def clarke_and_wright(path):
     # --------------- Dati del problema ---------------
-    instance = parser.make_instance_from_path_name(path)  # todo controllo se funziona
-    demands = parser.get_node_demands(instance)
-    truck_capacity = parser.get_truck(instance).capacity
-    edges = parser.get_edge_weight(instance)
+    instance = Parser.make_instance_from_path_name(path)  # todo controllo se funziona
+    demands = Parser.get_node_demands(instance)  # Ci sono anche le domande del deposito
+    truck_capacity = Parser.get_truck(instance).capacity
+    edges = Parser.get_edge_weight(instance)  # Pesi degli archi
+    for e in edges:
+        print(e)
+    depots = Parser.get_depots_index(instance)  # Indici dei depositi
+    depot_index = depots[0]  # todo !!!Assunto inizialmente un solo deposito, per semplicità di ragionamento!!
     # -------------- Variabili da calcolare --------------
-    routes = []  # Inizializzo le route come vuote
-    cw_cost = 0
+    routes = []  # Inizializzo le route come vuote (inizialmente ogni cliente ha un proprio veicolo, n route)
+    cw_cost = 0  # Variabile che esprime il costo totale dei percorsi
     # -----------------------------------------------------------------------------------
     # Passo 0: Devo definire un cammino per ogni cliente: (depotIndex, i, depotIndex)
-    n = parser.get_nodes_dimension(instance)
-
-    depots = parser.get_depots_index(instance)
-    depot_index = depots[0]  # !!!!! Assunto inizialmente un solo deposito, per semplicità di ragionamento
-
-    for i in range(1, n):
-        routes.append([depot_index, i, depot_index])
-        cw_cost += edges[depot_index][i] + edges[i][depot_index]
-    print(f"Routes: {routes}")
-    print(f"Initial Cost: {cw_cost}")
+    n = Parser.get_nodes_dimension(instance)
+    for i in range(0, n):
+        if i not in depots:  # Se il cliente i non è un deposito
+            routes.append([depot_index, i, depot_index])
+            cw_cost += edges[depot_index][i] + edges[i][depot_index]
+    print(f"First n Route: {routes} \nCost {cw_cost}")
     # -----------------------------------------------------------------------------------
     # Passo 1: Calcolo saves per ogni coppia di nodi e li ordino in modo decrescente
     saves = calculate_saves_and_sort_descent(instance)
-    print("Saves:")
+    print("Saves Ordered:")
     for s in saves:
         print(s)
     # -----------------------------------------------------------------------------------
-    # Passo 2: Unisco le route in modo ammissibile
+    # Passo 2: Unisco le route in modo ammissibile (Provo solo save positivi)
     for s in saves:
-        i, j, _ = s
-        if merge_routes_if_possible(routes, i, j, demands, truck_capacity, edges):
-            print(f"Routes merged: {i} and {j}")
-            cw_cost -= s[2]
-            print(f"Updated Cost: {cw_cost}")
+        i, j, save_value = s
+        if save_value >= 0:
+            if merge_routes_if_possible(routes, i, j, demands, truck_capacity):
+                print(f"Merged {i} and {j} in the same route with save {save_value}")
+                cw_cost -= save_value
+                print(f"Updated Cost: {cw_cost}")
+        # Se il save è negativo, non ha senso unire i percorsi
     # -----------------------------------------------------------------------------------
     # Passo 3: salva il risultato in un file .sol e stampa i risultati
     save_results_to_file(routes, cw_cost, path)
-    # Stampa i risultati
+    # Stampa i risultati a schermo
     for index, route in enumerate(routes):
         route_str = " ".join(str(node) for node in route[1:-1])  # Escludi l'ID del deposito
-        print(f"Route #{index + 1}: {route_str}")
+        total_demand = sum(demands[node] for node in route[1:-1])  # Escludi il deposito
+        print(
+            f"Route #{index + 1}: {route_str} |total demand: {total_demand} |route cost: {sum(edges[route[i]][route[i + 1]] for i in range(len(route) - 1))}")
     print(f"Cost {cw_cost}")
 
 
 # -------------------------- Test -----------------------------
-clarke_and_wright("resources/vrplib/Instances/P-n16-k8.vrp")
+clarke_and_wright("resources/vrplib/Instances/A-n32-k5.vrp")
+#clarke_and_wright("resources/vrplib/Instances/CopilotInstance.vrp")
