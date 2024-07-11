3771a2b72f7a7aa8e9c1eb75911dee65ae9c8816
andreatozzi99 <andreatozzi199@gmail.com>
2024-07-11 10:47:28 +0200

    Ora la generazione dei dat dovrebbe essere corretta

--- src/main/VRP_to_DAT.py
+++ src/main/VRP_to_DAT.py
@@ -1,64 +1,62 @@
 import os
-import vrplib
-
+import ParseInstances as Parser
 
 FILES_DIRECTORY = '../resources/vrplib/Instances'
 OUTPUT_DIRECTORY = '../resources/vrplib/DATs'
-# Path della directory dei file dats (servirà ad ampl)
-# C:\Users\andre\PycharmProjects\Progetto_AMOD\src\resources\vrplib\DATs
 
 
 def parse_vrp(file_path):
-    instance = vrplib.read_instance(file_path)
+    instance = Parser.make_instance_from_path_name(file_path)
 
     # Estrazione dei dati necessari dall'istanza
-    name = instance.get('name')
-    capacity = instance.get('capacity')
-    demands = instance.get('demand').tolist()
-    nodes = instance.get('node_coord').tolist()
-    depot = instance.get('depot')[0]
+    name = Parser.get_name(instance)
+    capacity = Parser.get_truck_capacity(instance)
+    demands = Parser.get_node_demands(instance)
+    num_nodes = Parser.get_nodes_dimension(instance)  # Lista delle coordinate dei nodi
+    depot = Parser.get_depots_index(instance)[0]
+    weight = Parser.get_edge_weight(instance)
 
     return {
         'name': name,
         'capacity': capacity,
+        'num_vehicles': Parser.get_truck(instance).get_min_num(),
         'demands': demands,
-        'nodes': nodes,
+        'num_nodes': num_nodes,
         'depot': depot,
-        'edge_weight': instance.get('edge_weight')
+        'edge_weight': weight
     }
 
 
 def generate_ampl_data_from_vrp(vrp_data, output_file):
     with open(output_file, 'w') as file:
         file.write("data;\n\n")
+        num_nodes = vrp_data['num_nodes']
+        num_vehicles = vrp_data['num_vehicles']
 
         # Set di vertici
-        file.write("set V := " + " ".join(str(i + 1) for i in range(len(vrp_data['nodes']))) + ";\n")
-        file.write("set V_CUST := " + " ".join(str(i + 1) for i in range(len(vrp_data['nodes']))
-                                               if i + 1 != vrp_data['depot']) + ";\n")
+        file.write(f"set V := {' '.join(str(i + 1) for i in range(num_nodes))};\n")
 
         # Set di veicoli
-        num_vehicles = sum(vrp_data['demands']) // vrp_data['capacity'] + 1
-        file.write(f"set K := {' '.join(str(i) for i in range(1, num_vehicles + 1))};\n")
+        file.write(f"set K := {' '.join(str(i) for i in range(1, num_vehicles+1))};\n")
 
         # Capacità
         file.write(f"param C := {vrp_data['capacity']};\n")
 
         # Domanda dei clienti
-        file.write("param d :=\n")
-        for i, demand in enumerate(vrp_data['demands']):
-            file.write(f" {i + 1} {demand}\n")
+        file.write("param d :\n")
+        for i, d in enumerate(vrp_data['demands']):
+            file.write(f" {i + 1} {d}\n")
         file.write(";\n")
 
         # Distanze (costi di percorrenza)
-        file.write("param c:\n")
-        file.write(" " + " ".join(str(i + 1) for i in range(len(vrp_data['nodes']))) + " :=\n")
+        file.write("param c :\n")
+        file.write(" ")
+        file.write(" ".join(str(i + 1) for i in range(num_nodes)) + " :=\n")
 
-        for i, node_i in enumerate(vrp_data['nodes']):
+        for i in range(num_nodes):
             file.write(f"{i + 1} ")
-            for j, node_j in enumerate(vrp_data['nodes']):
-                dist = ((node_i[0] - node_j[0]) ** 2 + (node_i[1] - node_j[1]) ** 2) ** 0.5
-                file.write(f" {dist}")
+            for j in range(num_nodes):
+                file.write(f" {vrp_data['edge_weight'][i][j]}")
             file.write("\n")
         file.write(";\n")
 
@@ -78,4 +76,15 @@ def Generate_All_Dats_from_vrp(input_dir, output_dir):
             print(f"Generated {output_file}")
 
 
-Generate_All_Dats_from_vrp(FILES_DIRECTORY, OUTPUT_DIRECTORY)
+def Generate_Dat_from_vrp(input_file, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    vrp_data = parse_vrp(input_file)
+    output_file = os.path.join(output_dir, f"{vrp_data['name']}.dat")
+    generate_ampl_data_from_vrp(vrp_data, output_file)
+    print(f"Generated {output_file}")
+
+
+Generate_Dat_from_vrp('../resources/vrplib/Instances/A-n32-k5.vrp', OUTPUT_DIRECTORY)
+#Generate_All_Dats_from_vrp(FILES_DIRECTORY, OUTPUT_DIRECTORY)
