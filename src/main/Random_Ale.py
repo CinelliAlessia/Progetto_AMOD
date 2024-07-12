4d78a68489c7e15284e67b21c6ff4941ea6ca7af
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-12 16:10:11 +0200

    Update, versione semi definitiva

--- src/main/Random_Ale.py
+++ src/main/Random_Ale.py
@@ -1,71 +1,71 @@
 from random import shuffle
 
 
-def vrp_random_1(nodes, capacity):
+def vrp_random(nodes, capacity, demands, id_depots):
+    """ Preso un client a caso, controlla se puo essere aggiunto al primo cluster, se cosi non è,
+    controlla se puo essere aggiunto al secondo cluster, ecc. se non puo essere aggiunto a nessun cluster
+    se ne crea uno nuovo
+    :param id_depots: indice del deposito
+    :param demands: domanda totale di tutti i clienti
+    :param nodes: lista di clienti
+    :param capacity: capacità dei veicoli
+    :return:
+    """
+
+    nodes = nodes[:id_depots] + nodes[id_depots + 1:]  # Rimuove il deposito dalla lista dei clienti
+
+    truck_max = int(demands/capacity) + 1
+    rem_capacities = [0 for _ in range(truck_max)]
+
+    shuffle(nodes)  # Randomizza la lista dei clienti
+    routes = []
+
+    for client in nodes:
+        for i, route in enumerate(routes):
+            if rem_capacities[i] + client.get_demand() <= capacity:
+                route.append(client)
+                break
+        else:
+            routes.append([client])     # Crea una nuova rotta se non può essere aggiunto a nessun cluster
+
+    processed_roots = []
+    for route in routes:
+        if route:  # Controlla se la rotta non è vuota e aggiunge il deposito all'inizio e alla fine
+            processed_root = [id_depots] + [c.get_id() for c in route] + [id_depots]
+            processed_roots.append(processed_root)
+
+    # TODO calcola e restituisci il costo
+    return processed_roots
+
+
+def vrp_random_alternative(nodes, capacity, demands, id_depots):
     """ Prendo un cliente a caso,
     se la sua domanda è soddisfatta lo aggiungo al cluster, altrimenti continuo a cercare,
     se nessun cliente può essere aggiunto a quel determinato cluster allora lo chiudo e ne apro un altro"""
 
-    id_depots = 0  # Assuming depot ID is 0 for simplicity
-    nodes = [node for node in nodes if node.get_id() != id_depots]  # Remove depot from nodes
+    nodes = nodes[:id_depots] + nodes[id_depots + 1:]  # Rimuove il deposito dalla lista dei clienti
+    shuffle(nodes)  # Randomizza la lista dei clienti
 
-    shuffle(nodes)  # Shuffle nodes for random selection
+    truck_max = int(demands/capacity) + 1
 
     # List to store all clusters inizialmente n clusters vuoti
-    roots = [[] for _ in range(len(nodes))]
+    roots = [[] for _ in range(truck_max)]
+    rem_capacities = [0 for _ in range(len(nodes))]
 
-    for root in roots:
+    for i, root in enumerate(roots):
         for client in nodes:
-            if check_capacity(root, capacity, client):
+            if rem_capacities[i] + client.get_demand() <= capacity:
+                rem_capacities[i] += client.get_demand()
                 root.append(client)
                 nodes.remove(client)
-        else:
-            continue
 
-    processed_roots = []
-    for root in roots:
-        if root:  # Check if the root is not empty
-            processed_root = [id_depots] + [c.get_id() for c in root] + [id_depots]
-            processed_roots.append(processed_root)
-
-    return processed_roots
-
-
-def vrp_random_2(nodes, capacity):
-    """ Preso un client a caso, controlla se puo essere aggiunto al primo cluster, se cosi non è,
-    controlla se puo essere aggiunto al secondo cluster, ecc. se non puo essere aggiunto a nessun cluster
-    se ne crea uno nuovo
-    :param nodes:
-    :param capacity:
-    :return:
-    """
-
-    id_depots = 0  # Assuming depot ID is 0 for simplicity
-    nodes = [node for node in nodes if node.get_id() != id_depots]  # Remove depot from nodes
-
-    shuffle(nodes)  # Shuffle nodes for random selection
-    roots = []
-
-    for client in nodes:
-        for root in roots:
-            if check_capacity(root, capacity, client):
-                root.append(client)
+            if rem_capacities[i] == capacity:
                 break
-        else:
-            roots.append([client])
 
     processed_roots = []
     for root in roots:
-        if root:  # Check if the root is not empty
+        if root:  # Controlla se la rotta non è vuota e aggiunge il deposito all'inizio e alla fine
             processed_root = [id_depots] + [c.get_id() for c in root] + [id_depots]
             processed_roots.append(processed_root)
 
     return processed_roots
-
-
-def check_capacity(cluster, capacity, new_client):
-    if len(cluster) > 0:
-        current_capacity = sum(client.get_demand() for client in cluster)
-        return current_capacity + new_client.get_demand() <= capacity
-    else:
-        return True
