2c39550d008c826a4b65fd630db593e9a47489d2
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-12 16:10:36 +0200

    Correzione warning

--- src/main/Start.py
+++ src/main/Start.py
@@ -1,12 +1,11 @@
-import os
 import time
 import Clarke_Wright_Alessia as cwAle
 import Clarke_Wright_Andrea as Cw
 import ParseInstances as Parse
-import Plotter as plotter
+import Plotter as Plotty
 import Sweep_Ale as sweepAle
 from src.main import Utils
-from src.main.Random_Ale import vrp_random_1, vrp_random_2
+from src.main.Random_Ale import vrp_random
 from src.main.Sweep_Andrea import solve_sweep_on_instance
 
 
@@ -15,18 +14,18 @@ def print_roots(roots):
         print(r)
 
 
-def plot_if_not_explicit(nodes, roots):
+def plot_if_not_explicit(roots):
     if nodes[0].get_x() is not None:
-        plotter.plot_roots_graph(nodes, roots)
+        Plotty.plot_roots_graph(nodes, roots)
     else:
         print("L'istanza ha formato 'EXPLICIT', non è possibile visualizzare i nodi su un grafico.")
 
 
-CW_ALE = True
+CW_ALE = False
 CW_ANDRE = False
 SWEEP_ALE = False
 SWEEP_ANDRE = False
-RANDOM = False
+RANDOM = True
 
 if CW_ALE or CW_ANDRE:
     work_on_explicit = True
@@ -34,132 +33,117 @@ else:
     work_on_explicit = False
 
 
-path_instance = "../resources/vrplib/Instances/P-n16-k8.vrp"
+path_instance = "../resources/vrplib/Instances/X-n670-k130.vrp"
 instance = Parse.make_instance_from_path_name(path_instance)
 nodes, truck = Parse.work_on_instance(instance, work_on_explicit)
 
-if nodes is None:
-    # Non continuare l'esecuzione
-    exit(1)
 
-# ALESSIA CW
-if CW_ALE:
-    # Registra il tempo di inizio
-    start_time = time.time()
-    # Chiamata alla funzione che vuoi misurare
-    roots = cwAle.start(nodes, truck)
-    # Registra il tempo di fine
-    end_time = time.time()
-    # Calcola la durata dell'esecuzione
-    execution_time = end_time - start_time
-    print(f"Tempo di esecuzione cwAle: {execution_time} secondi")
+def start():
+    if nodes is None:
+        return None
 
-    plot_if_not_explicit(nodes, roots)
-    cost = Utils.calculate_cost(roots, nodes)
-    print_roots(roots)
-    print(f"Costi CW ALESSIA {cost}")
+    # ALESSIA CW
+    if CW_ALE:
+        # Registra il tempo di inizio
+        start_time = time.time()
+        # Chiamata alla funzione che vuoi misurare
+        roots = cwAle.start(nodes, truck)
+        # Registra il tempo di fine
+        end_time = time.time()
+        # Calcola la durata dell'esecuzione
+        execution_time = end_time - start_time
+        print(f"Tempo di esecuzione cwAle: {execution_time} secondi")
 
-# ANDREA CW
-if CW_ANDRE:
-    # Registra il tempo di inizio
-    start_time = time.time()
-    # Chiamata alla funzione che vuoi misurare
-    _, roots = Cw.solve_clarke_and_wright_on_instance(instance)
-    # Registra il tempo di fine
-    end_time = time.time()
-    # Calcola la durata dell'esecuzione
-    execution_time = end_time - start_time
-    print(f"Tempo di esecuzione cwAndre: {execution_time} secondi")
-
-    plot_if_not_explicit(nodes, roots)
-
-    cost = Utils.calculate_cost(roots, nodes)
-    print_roots(roots)
-    print(f"Costi CW ANDREA {cost}")
-
-# SWEEP ALESSIA
-if SWEEP_ALE:
-    # Registra il tempo di inizio
-    start_time = time.perf_counter()
-    # Chiamata alla funzione che vuoi misurare
-    roots_1, roots_2, roots_3 = sweepAle.sweep_algorithm(nodes, truck.get_capacity())
-
-    # Registra il tempo di fine
-    end_time = time.perf_counter()
-    # Calcola la durata dell'esecuzione
-    execution_time = end_time - start_time
-    print(f"Tempo di esecuzione sweep Ale: {execution_time} secondi")
-
-    plot_if_not_explicit(nodes, roots_2)
-
-    cost_2 = Utils.calculate_cost(roots_2, nodes)
-    print_roots(roots_2)
-    print(f"Costi SWEEP ALESSIA opt2: {cost_2}")
-
-    plot_if_not_explicit(nodes, roots_3)
-
-    cost_3 = Utils.calculate_cost(roots_3, nodes)
-    print_roots(roots_3)
-    print(f"Costi SWEEP ALESSIA opt3: {cost_3}")
-
-# SWEEP ANDREA
-if SWEEP_ANDRE:
-    # Registra il tempo di inizio
-    start_time = time.perf_counter()
-    # Chiamata alla funzione che vuoi misurare
-    roots, _ = solve_sweep_on_instance(instance, True, False)
-    # Registra il tempo di fine
-    end_time = time.perf_counter()
-    # Calcola la durata dell'esecuzione
-    execution_time = end_time - start_time
-    print(f"Tempo di esecuzione sweep Andrea: {execution_time} secondi")
-
-    plot_if_not_explicit(nodes, roots)
-
-    cost = Utils.calculate_cost(roots, nodes)
-    print_roots(roots)
-    print(f"Costi SWEEP ANDREA {cost}")
-
-if RANDOM:
-
-    best_cost = float("inf")
-    best_root = []
-
-    # Registra il tempo di inizio
-    start_time = time.perf_counter()
-    for i in range(1000):
-        roots = vrp_random_1(nodes, truck.get_capacity())
+        plot_if_not_explicit(roots)
         cost = Utils.calculate_cost(roots, nodes)
-        if cost < best_cost:
-            best_cost = cost
-            best_root = roots  # Registra il tempo di fine
-    end_time = time.perf_counter()
+        print_roots(roots)
+        print(f"Costi CW ALESSIA {cost}")
 
-    # Calcola la durata dell'esecuzione
-    execution_time = end_time - start_time
-    print(f"Tempo di esecuzione random 1: {execution_time} secondi")
+    # ANDREA CW
+    if CW_ANDRE:
+        # Registra il tempo di inizio
+        start_time = time.time()
+        # Chiamata alla funzione che vuoi misurare
+        _, roots = Cw.solve_clarke_and_wright_on_instance(instance)
+        # Registra il tempo di fine
+        end_time = time.time()
+        # Calcola la durata dell'esecuzione
+        execution_time = end_time - start_time
+        print(f"Tempo di esecuzione cwAndre: {execution_time} secondi")
 
-    plot_if_not_explicit(nodes, best_root)
+        plot_if_not_explicit(roots)
 
-    print_roots(best_root)
-    print(f"Costi random_1 {best_cost}")
-
-    best_cost = float("inf")
-    best_root = []
-
-    # Registra il tempo di inizio
-    start_time = time.perf_counter()
-    for i in range(1000):
-        roots = vrp_random_2(nodes, truck.get_capacity())
         cost = Utils.calculate_cost(roots, nodes)
-        if cost < best_cost:
-            best_cost = cost
-            best_root = roots  # Registra il tempo di fine
-    end_time = time.perf_counter()
-    # Calcola la durata dell'esecuzione
-    execution_time = end_time - start_time
-    print(f"Tempo di esecuzione random 2: {execution_time} secondi")
+        print_roots(roots)
+        print(f"Costi CW ANDREA {cost}")
 
-    plot_if_not_explicit(nodes, best_root)
-    print_roots(best_root)
-    print(f"Costi random_2 {best_cost}")
+    # SWEEP ALESSIA
+    if SWEEP_ALE:
+        # Registra il tempo di inizio
+        start_time = time.perf_counter()
+        # Chiamata alla funzione che vuoi misurare
+        roots_1, roots_2, roots_3 = sweepAle.sweep_algorithm(nodes, truck.get_capacity())
+
+        # Registra il tempo di fine
+        end_time = time.perf_counter()
+        # Calcola la durata dell'esecuzione
+        execution_time = end_time - start_time
+        print(f"Tempo di esecuzione sweep Ale: {execution_time} secondi")
+
+        plot_if_not_explicit(roots_2)
+
+        cost_2 = Utils.calculate_cost(roots_2, nodes)
+        print_roots(roots_2)
+        print(f"Costi SWEEP ALESSIA opt2: {cost_2}")
+
+        plot_if_not_explicit(roots_3)
+
+        cost_3 = Utils.calculate_cost(roots_3, nodes)
+        print_roots(roots_3)
+        print(f"Costi SWEEP ALESSIA opt3: {cost_3}")
+
+    # SWEEP ANDREA
+    if SWEEP_ANDRE:
+        # Registra il tempo di inizio
+        start_time = time.perf_counter()
+        # Chiamata alla funzione che vuoi misurare
+        roots, _ = solve_sweep_on_instance(instance, True, False)
+        # Registra il tempo di fine
+        end_time = time.perf_counter()
+        # Calcola la durata dell'esecuzione
+        execution_time = end_time - start_time
+        print(f"Tempo di esecuzione sweep Andrea: {execution_time} secondi")
+
+        plot_if_not_explicit(roots)
+
+        cost = Utils.calculate_cost(roots, nodes)
+        print_roots(roots)
+        print(f"Costi SWEEP ANDREA {cost}")
+
+    if RANDOM:
+        total_demand = Utils.total_demand(nodes)
+        id_depots = Parse.get_depots_index(instance)[0]
+
+        best_cost = float("inf")
+        best_root = []
+
+        # Registra il tempo di inizio
+        start_time = time.perf_counter()
+        for i in range(1000):
+            roots = vrp_random(nodes, truck.get_capacity(), total_demand, id_depots)
+            cost = Utils.calculate_cost(roots, nodes)
+            if cost < best_cost:
+                best_cost = cost
+                best_root = roots  # Registra il tempo di fine
+        end_time = time.perf_counter()
+
+        # Calcola la durata dell'esecuzione
+        execution_time = end_time - start_time
+
+        print_roots(best_root)
+        plot_if_not_explicit(best_root)
+        print(f"Costi random: {best_cost}")
+        print(f"Tempo di esecuzione random: {execution_time} secondi")
+
+
+start()
