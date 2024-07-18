aee58f8e63a00b1c1eecb4ef47a8a2fb65ca2854
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-18 08:42:32 +0200

    Update

--- main/Solve_Random_4All.py
+++ main/Solve_Random_4All.py
@@ -6,7 +6,7 @@ import os
 import Utils
 
 
-ACTIONS = False
+ACTIONS = True
 
 if ACTIONS:
     # Directory dei file contenenti i nomi delle istanze
--- main/Solve_Sweep_4All.py
+++ main/Solve_Sweep_4All.py
@@ -24,7 +24,7 @@ OPT_3 = True
 
 # ------------------------------------------------------------------------------------------------------------
 
-ACTIONS = False
+ACTIONS = True
 
 if ACTIONS:
     # Esegui l'euristica per tutte le size delle istanze GITHUB ACTION
