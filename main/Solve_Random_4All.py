ddfa185e78c4586b56b24708e71dbf83a44c0967
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-22 15:29:39 +0200

    run Random

--- main/Solve_Random_4All.py
+++ main/Solve_Random_4All.py
@@ -7,12 +7,12 @@ import os
 import Utils
 
 # Se impostati a True, eseguirà l'euristica di Clarke e Wright per le istanze di quel tipo
-SMALL = True
-MID_SMALL = True
-MID = True
-MID_LARGE = True
-LARGE = False
-X_LARGE = False
+SMALL = False
+MID_SMALL = False
+MID = False
+MID_LARGE = False
+LARGE = True
+X_LARGE = True
 
 # ------------------------------------------------------------------------------------------------------------
 
