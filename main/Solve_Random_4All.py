cc7efb20f37c18b5d1a95e275b3d581b4ea37120
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-24 10:46:33 +0200

    Random small 1M

--- main/Solve_Random_4All.py
+++ main/Solve_Random_4All.py
@@ -33,7 +33,7 @@ else:
     INSTANCES_DIRECTORY = "../resources/vrplib/Instances/"  # Directory delle istanze
 
 OUTPUT_BASE_FILE_NAME = "RANDOM_APX_and_Time"  # Aggiungere come prefisso il numero del run
-RANDOM_ITERATION_NUMBER = 100000
+RANDOM_ITERATION_NUMBER = 1000000
 
 
 def solve_random_for_instance_name_in_file(size, file_path):
