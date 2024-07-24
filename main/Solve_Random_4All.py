0db0431c85b9188b8c605c191815a47195868ad2
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-24 14:54:13 +0200

    mid random 5 min

--- main/Solve_Random_4All.py
+++ main/Solve_Random_4All.py
@@ -7,8 +7,8 @@ import Config
 
 # Se impostati a True, eseguirà l'euristica di Clarke e Wright per le istanze di quel tipo
 SMALL = False
-MID_SMALL = True
-MID = False
+MID_SMALL = False
+MID = True
 MID_LARGE = False
 LARGE = False
 X_LARGE = False
