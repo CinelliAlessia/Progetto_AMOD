b3e0ce0f2ee02095262b248599b3a81f5c90db11
Alessia Cinelli <cinellialessia@gmail.com>
2024-07-24 10:43:49 +0200

    Random 1000 xlarge

--- main/Solve_Random_4All.py
+++ main/Solve_Random_4All.py
@@ -7,12 +7,12 @@ import Config
 
 
 # Se impostati a True, eseguirà l'euristica di Clarke e Wright per le istanze di quel tipo
-SMALL = True
-MID_SMALL = True
-MID = True
-MID_LARGE = True
-LARGE = True
-X_LARGE = False
+SMALL = False
+MID_SMALL = False
+MID = False
+MID_LARGE = False
+LARGE = False
+X_LARGE = True
 
 # ------------------------------------------------------------------------------------------------------------
 ACTIONS = Config.ACTION_RANDOM
